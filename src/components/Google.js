04132407dd2c99f22ef9db3c25f2c71fe0b20405
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-10-21 11:03:12 -0600

    working google search

---
+++ src/components/Google.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class Google extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+
+    onChange(e) {
+        this.setState({
+            query: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        if (e.key === 'Enter') {
+            console.log('Submit');
+            let q = this.state.query;
+            console.log('q:', q);
+            window.open('http://google.com/search?q=' + q);
+        }
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="input-field col s12">
+                    <i className="material-icons prefix">search</i>
+                    <input id="google-search" type="text" onChange={this.onChange.bind(this)} onKeyPress={this.onSubmit.bind(this)} />
+                    <label htmlFor="google-search">Google Search...</label>
+                </div>
+            </div>
+        )
+    }
+}
