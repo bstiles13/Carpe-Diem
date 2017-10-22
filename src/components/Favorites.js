b4d57e1b8bcff9f775ba56d37b4fd387d380c877
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-10-21 22:19:33 -0600

    favorites styling

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -10,49 +10,49 @@ export default class Favorites extends React.Component {
                 {
                     category: 'Social',
                     pages: [
-                        { name: 'Facebook', url: 'https://www.facebook.com/' },
-                        { name: 'Twitter', url: 'https://twitter.com/' },
-                        { name: 'LinkedIn', url: 'https://www.linkedin.com/' }
+                        { order: 0, name: 'Facebook', url: 'https://www.facebook.com/' },
+                        { order: 1, name: 'Twitter', url: 'https://twitter.com/' },
+                        { order: 2, name: 'LinkedIn', url: 'https://www.linkedin.com/' }
                     ]
                 },
                 {
                     category: 'News',
                     pages: [
-                        { name: 'Yahoo', url: 'https://www.yahoo.com/' },
-                        { name: 'CNN', url: 'http://www.cnn.com/' },
-                        { name: 'Huffington Post', url: 'https://www.huffingtonpost.com/' }
+                        { order: 3, name: 'Yahoo', url: 'https://www.yahoo.com/' },
+                        { order: 4, name: 'CNN', url: 'http://www.cnn.com/' },
+                        { order: 5, name: 'Huffington Post', url: 'https://www.huffingtonpost.com/' }
                     ]
                 },
                 {
                     category: 'Email',
                     pages: [
-                        { name: 'Google Mail', url: 'https://mail.google.com/' },
-                        { name: 'Yahoo Mail', url: 'https://mail.yahoo.com/' },
-                        { name: 'Outlook', url: 'https://outlook.live.com/' }
+                        { order: 6, name: 'Google Mail', url: 'https://mail.google.com/' },
+                        { order: 7, name: 'Yahoo Mail', url: 'https://mail.yahoo.com/' },
+                        { order: 8, name: 'Outlook', url: 'https://outlook.live.com/' }
                     ]
                 },
                 {
                     category: 'Finance',
                     pages: [
-                        { name: 'Chase', url: 'https://www.chase.com/' },
-                        { name: 'Bank of America', url: 'https://www.bankofamerica.com/' },
-                        { name: 'Wells Fargo', url: 'https://www.wellsfargo.com/' }
+                        { order: 9, name: 'Chase', url: 'https://www.chase.com/' },
+                        { order: 10, name: 'Bank of America', url: 'https://www.bankofamerica.com/' },
+                        { order: 11, name: 'Wells Fargo', url: 'https://www.wellsfargo.com/' }
                     ]
                 },
                 {
                     category: 'Shop',
                     pages: [
-                        { name: 'Amazon', url: 'https://www.amazon.com/' },
-                        { name: 'Walmart', url: 'https://www.walmart.com/' },
-                        { name: 'eBay', url: 'https://www.ebay.com/' }
+                        { order: 12, name: 'Amazon', url: 'https://www.amazon.com/' },
+                        { order: 13, name: 'Walmart', url: 'https://www.walmart.com/' },
+                        { order: 14, name: 'eBay', url: 'https://www.ebay.com/' }
                     ]
                 },
                 {
                     category: 'Watch',
                     pages: [
-                        { name: 'Netflix', url: 'https://www.netflix.com/' },
-                        { name: 'Hulu', url: 'https://www.hulu.com/' },
-                        { name: 'Amazon Video', url: 'https://www.primevideo.com/' }
+                        { order: 15, name: 'Netflix', url: 'https://www.netflix.com/' },
+                        { order: 16, name: 'Hulu', url: 'https://www.hulu.com/' },
+                        { order: 17, name: 'Amazon Video', url: 'https://www.primevideo.com/' }
                     ]
                 }
             ]
@@ -66,19 +66,21 @@ export default class Favorites extends React.Component {
                 // let domain = url.indexOf('//www') === -1 ? url.split('//')[1].split('/')[0] : url.split('//www.')[1].split('/')[0];
                 // domain = domain.substring(0, domain.lastIndexOf('.')).toUpperCase();
                 return (
-                    <div>
-                        <div className="chip">
-                            <img className="url-logo" src={'//logo.clearbit.com/spotify.com' + page.url} />
-                            {page.name}
-                        </div>
-                        <br />
-                    </div>
+                    <li className='custom-item'>
+                        <img className="url-logo circle" src={'//logo.clearbit.com/spotify.com' + page.url} />
+                        <span>{page.name}</span>
+                        <a className='dropdown-button' href='#' data-activates={page.order}><i className="material-icons">arrow_drop_down</i></a>
+                        <ul id={page.order} className='dropdown-content'>
+                            <li><a href="#!"><i className="material-icons">edit</i>Edit</a></li>
+                            <li><a href="#!"><i className="material-icons">delete</i>Remove</a></li>
+                        </ul>
+                    </li>
                 )
             })
             return (
                 <div className='custom-card'>
-                    <h5>{favorite.category}</h5>
-                    {pages}
+                    <div className='custom-title'>{favorite.category}</div>
+                    <ul className='custom-list'>{pages}</ul>
                 </div>
             )
         })
